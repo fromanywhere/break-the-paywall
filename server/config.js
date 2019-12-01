66a13e1c6efb8a8114796ce067fc08e968e8ea2b
Ivan Metelev <ivan.metelev@corp.mail.ru>
2019-12-01 22:51:07 +0300

    Readme explanation

--- server/config.js
+++ server/config.js
@@ -1,7 +1,12 @@
 module.exports = {
     port: 80,
-    path: "",
-    alias: "",
+    path: "/absolute/path/on/your/filesystem",
+    alias: "http://your-base-url/alias-path/",
+    token: [
+        "<approved>",
+        "<token>",
+        "<list>"
+    ],
     rules: {
         "republic.ru": [
             {
@@ -25,7 +30,25 @@ module.exports = {
                     ".panel"
                 ]
             }
+        ],
+        "medium.com": [
+            {
+                type: "replace",
+                find: 'noscript',
+                replace: 'div'
+            },
+            {
+                type: "remove-by-selector",
+                selectors: [
+                    "iframe",
+                    "script",
+                    "noscript",
+                    "[aria-modal='true']",
+                    "img[src*='?q=20']",
+                    "link[as='script']",
+                    ".branch-journeys-top"
+                ]
+            }
         ]
-    },
-    token: []
+    }
 };
